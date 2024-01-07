efa67182133f0ee1c08d0c473f6bed617df650c3
Hossein <farhadiasghar125@gmail.com>
2024-01-07 10:05:17 -0500

    Feat: Add main file

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    # open textbox and getting commands
+    # command proccessing
+    # sending command to textbox for showing it
+    pass
+
+
+if __name__ == "__main__":
+    main()
