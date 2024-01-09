1eb5c99d6c70d78af7f17d0d7ad229b9bd683244
Hossein <farhadiasghar125@gmail.com>
2024-01-09 02:34:52 -0500

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    # open textbox and getting commands
+    # command proccessing
+    # sending command to textbox for showing it
+    pass
+
+
+if __name__ == "__main__":
+    main()
